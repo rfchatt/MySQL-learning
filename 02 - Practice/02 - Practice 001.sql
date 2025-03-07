ad4920359e7bf2437587381d1c51c0b873eed034
Abderrafie <chattabdrrafie@gmail.com>
2025-03-07 17:40:47 +0000

    Practice 001

--- 02 - Practice/02 - Practice 001.sql
+++ 02 - Practice/02 - Practice 001.sql
@@ -8,25 +8,25 @@ USE ecole;
 -- creation de Table etudiant
 CREATE TABLE IF NOT EXISTS etudiant(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    age INT CHECK (age > 10)
+	nom VARCHAR(100) UNIQUE NOT NULL,
+    	email VARCHAR(100) UNIQUE NOT NULL,
+    	age INT CHECK (age > 10)
 ) ENGINE=InnoDB; 
 
 
 -- creation de Table Cours
 CREATE TABLE IF NOT EXISTS cour(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    titre VARCHAR(100) UNIQUE NOT NULL,
-    duree_heures INT NOT NULL CHECK (duree_heures > 0)
+    	titre VARCHAR(100) UNIQUE NOT NULL,
+    	duree_heures INT NOT NULL CHECK (duree_heures > 0)
 ) ENGINE=InnoDB;
 
 
 -- creation de Table inscription
 CREATE TABLE IF NOT EXISTS inscription(
 	etudiant_id INT,
-    cours_id INT,
-    date_inscription DATE,
+    	cours_id INT,
+    	date_inscription DATE,
     PRIMARY KEY (etudiant_id, cours_id),
     CONSTRAINT fk_etudiant_inscription FOREIGN KEY (etudiant_id) REFERENCES etudiants(id) ON
 		DELETE CASCADE ON UPDATE CASCADE,
