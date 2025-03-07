58c6cbcea384f78e6c946255aee56c5449f407d9
Abderrafie <chattabdrrafie@gmail.com>
2025-03-07 17:31:07 +0000

    DataBase Creation

--- 01 - Course /01 - Course 001.sql
+++ 01 - Course /01 - Course 001.sql
@@ -5,7 +5,7 @@
 
 -- 1) this line Create a DataBase named : Country
 -- [IF NOT EXISTS] is Optional to check if it's already Exists.
-CREATE DATABASE IF NOT EXISTS Country;
+CREATE DATABASE IF NOT EXISTS country;
 
 -- 2) CHARACTER SET [OPTIONAL] : defines the set of characters that can be stored in a database
 -- ex (utf8mb4) : Supports all Unicode characters including emojis.
@@ -14,11 +14,11 @@ CHARACTER SET utf8mb4;
 ----------------------------------------------------
 
 -- 3) this line Create a Table named : Students 
-CREATE TABLE Students(
+CREATE TABLE student(
   -- colonnes of Table :
 	idStudent INT PRIMARY KEY,         -- idStudent: name of 1st colonne | INT: type | PRIMARY KEY.
-  nameStudent VARCHAR(20) NOT NULL,  -- nameStudent: name of 2nd colonne | VARCHAR: type | NOT NULL.
-  age INT                            -- age: name of 3rd colonne | INT: type.
+ 	nameStudent VARCHAR(20) NOT NULL,  -- nameStudent: name of 2nd colonne | VARCHAR: type | NOT NULL.
+        age INT                            -- age: name of 3rd colonne | INT: type.
 ) ENGINE=InnoDB;
 
 ----------------------------------------------------
